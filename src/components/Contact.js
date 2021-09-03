97c7c82678852634609cc5b66582e86508535037
bobbykim89 <bobby.sihun.kim@gmail.com>
2021-09-03 11:00:43 -0700

    fixed minor issues, couldn't get the image form link provided, I need to request again

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,34 +1,69 @@
-import React from 'react'
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faVimeo, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import {
+  faVimeo,
+  faLinkedin,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
 
 export default function Contact() {
-    return (
-        <section className="text-white">
-            <div className="relative pt-16 pb-16 flex content-center bg-black md:items-center justify-center" style={{ minHeight: "40vh" }}>
-                <div className="relative container flex flex-col md:flex-row justify-center">
-                    <div className="mx-auto">
-                        <h3 className="font-bold text-5xl tracking-wider text-center mb-6">
-                            Contact
-                        </h3>
-                        <div className="text-center mb-6">
-                            <a href="mailto:nadianic.flores@gmail.com" target="_blank" rel="noreferrer" className="text-white inline-block hover:text-yellow-100 mb-4"><p className="text-xl">nadianic.flores@gmail.com</p></a>
-                            <p className="text-xl">+52 4424594251</p>
-                        </div>
-                        <div className="relative flex flex-wrap text-5xl font-bold justify-center">
-                            <div>
-                                <a href="https://www.linkedin.com/in/nadia-nicolas-flores-089ba695/" target="_blank" rel="noreferrer" className="text-white inline-block hover:text-green-500 mx-4"><FontAwesomeIcon icon={ faLinkedin } /></a>
-                            </div>
-                            <div>
-                                <a href="https://www.instagram.com/whitecatblackiee/?utm_medium=copy_link" target="_blank" rel="noreferrer" className="text-white inline-block hover:text-green-500 mx-4"><FontAwesomeIcon icon={ faInstagram } /></a>
-                            </div>
-                            <div>
-                                <a href="https://vimeo.com/user38033073" target="_blank" rel="noreferrer" className="text-white inline-block hover:text-green-500 mx-4"><FontAwesomeIcon icon={ faVimeo } /></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className='text-white' id='Contact'>
+      <div
+        className='relative pt-16 pb-16 flex content-center bg-black md:items-center justify-center'
+        style={{ minHeight: '40vh' }}
+      >
+        <div className='relative container flex flex-col md:flex-row justify-center'>
+          <div className='mx-auto'>
+            <h3 className='font-bold text-5xl tracking-wider text-center mb-6'>
+              Contact
+            </h3>
+            <div className='relative flex flex-wrap text-5xl font-bold justify-center'>
+              <div>
+                <a
+                  href='mailto:nadianic.flores@gmail.com'
+                  target='_blank'
+                  rel='noreferrer'
+                  className='text-white inline-block hover:text-green-500 mx-4'
+                >
+                  <FontAwesomeIcon icon={faEnvelopeSquare} />
+                </a>
+              </div>
+              <div>
+                <a
+                  href='https://www.linkedin.com/in/nadia-nicolas-flores-089ba695/'
+                  target='_blank'
+                  rel='noreferrer'
+                  className='text-white inline-block hover:text-green-500 mx-4'
+                >
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+              </div>
+              <div>
+                <a
+                  href='https://www.instagram.com/nadthez/'
+                  target='_blank'
+                  rel='noreferrer'
+                  className='text-white inline-block hover:text-green-500 mx-4'
+                >
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+              </div>
+              <div>
+                <a
+                  href='https://vimeo.com/user38033073'
+                  target='_blank'
+                  rel='noreferrer'
+                  className='text-white inline-block hover:text-green-500 mx-4'
+                >
+                  <FontAwesomeIcon icon={faVimeo} />
+                </a>
+              </div>
             </div>
-        </section>
-    )
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,58 +1,80 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 import Hamburger from 'hamburger-react';
 
-export default function Navbar () {
-    const [navBarOpen, setNavbarOpen] = useState (false);
+export default function Navbar() {
+  const [navBarOpen, setNavbarOpen] = useState(false);
 
-    return (
-        <nav id="Contact" className="w-screen top-0 absolute sticky items-center bg-black z-50">
-            <div className="container items-center mx-auto md:py-4 justify-between">
-                <div className="block flex justify-between md:hidden" >
-                    <div className="flex inline-block items-center ml-6">
-                        <a href="#Home" className="text-white text-2xl hover:text-yellow-200"><FontAwesomeIcon icon={ faHome } /></a>
-                    </div>
-                    <button className="flex items-center px-3 py-4 text-xl font-bold text-white hover:text-yellow-100" type="button" onClick={() => setNavbarOpen (!navBarOpen)}>
-                        <Hamburger size={ 23 } toggled={ navBarOpen } toggle={ setNavbarOpen } />
-                    </button>
-                </div>
-                <div 
-                    className={
-                        "flex-grow w-full lg:flex lg:w-auto flex-wrap items-center"
-                        +
-                        (navBarOpen ? "block" : " hidden")
-                    }
-                >
-                    <div className="flex pt-3 pb-4 md:pb-0 md:pt-1 md:flex-grow text-center justify-center">
-                        <a href="#Home" className="hidden md:block md:inline-block md:mx-auto text-white text-md md:text-xl hover:text-yellow-100 mr-4">
-                            <FontAwesomeIcon icon={ faHome } />
-                        </a>
-                        <a href="#About" className="block md:inline-block md:mx-auto text-white text-md md:text-xl hover:text-yellow-100 mr-4">
-                            ABOUT
-                        </a>
-                        <span className="inline-block md:hidden pt-1 text-white text-sm pr-4">
-                            |
-                        </span>
-                        <a href="#Skills" className="block md:inline-block md:mx-auto text-white text-md md:text-xl hover:text-yellow-100 mr-4">
-                            SOFTWARE
-                        </a>
-                        <span className="inline-block md:hidden pt-1 text-white text-sm pr-4">
-                            |
-                        </span>
-                        <a href="#Demo" className="block md:inline-block md:mx-auto text-white text-md md:text-xl hover:text-yellow-100 mr-4">
-                            WORKS
-                        </a>
-                        <span className="inline-block md:hidden pt-1 text-white text-sm pr-4">
-                            |
-                        </span>
-                        <a href="#Contact" className="block md:inline-block md:mx-auto text-white text-md md:text-xl hover:text-yellow-100 mr-4">
-                            CONTACT
-                        </a>
-                    </div>
-                </div>
-                
-            </div>
-        </nav>
-    );
+  return (
+    <nav className='w-screen top-0 absolute sticky items-center bg-black z-50'>
+      <div className='container items-center mx-auto md:py-4 justify-between'>
+        <div className='block flex justify-between md:hidden'>
+          <div className='flex inline-block items-center ml-6'>
+            <a
+              href='#Home'
+              className='text-white text-2xl hover:text-yellow-200'
+            >
+              <FontAwesomeIcon icon={faHome} />
+            </a>
+          </div>
+          <button
+            className='flex items-center px-3 py-4 text-xl font-bold text-white hover:text-yellow-100'
+            type='button'
+            onClick={() => setNavbarOpen(!navBarOpen)}
+          >
+            <Hamburger size={23} toggled={navBarOpen} toggle={setNavbarOpen} />
+          </button>
+        </div>
+        <div
+          className={
+            'flex-grow w-full lg:flex lg:w-auto flex-wrap items-center' +
+            (navBarOpen ? 'block' : ' hidden')
+          }
+        >
+          <div className='flex pt-3 pb-4 md:pb-0 md:pt-1 md:flex-grow text-center justify-center'>
+            <a
+              href='#Home'
+              className='hidden md:block md:inline-block md:mx-auto text-white text-md md:text-xl hover:text-yellow-100 mr-4'
+            >
+              <FontAwesomeIcon icon={faHome} />
+            </a>
+            <a
+              href='#About'
+              className='block md:inline-block md:mx-auto text-white text-md md:text-xl hover:text-yellow-100 mr-4'
+            >
+              ABOUT
+            </a>
+            <span className='inline-block md:hidden pt-1 text-white text-sm pr-4'>
+              |
+            </span>
+            <a
+              href='#Skills'
+              className='block md:inline-block md:mx-auto text-white text-md md:text-xl hover:text-yellow-100 mr-4'
+            >
+              SOFTWARE
+            </a>
+            <span className='inline-block md:hidden pt-1 text-white text-sm pr-4'>
+              |
+            </span>
+            <a
+              href='#Demo'
+              className='block md:inline-block md:mx-auto text-white text-md md:text-xl hover:text-yellow-100 mr-4'
+            >
+              WORKS
+            </a>
+            <span className='inline-block md:hidden pt-1 text-white text-sm pr-4'>
+              |
+            </span>
+            <a
+              href='#Contact'
+              className='block md:inline-block md:mx-auto text-white text-md md:text-xl hover:text-yellow-100 mr-4'
+            >
+              CONTACT
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
